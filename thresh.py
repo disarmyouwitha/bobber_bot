055b52690abcd055210e9a491d9edadd16402c71
nap <email@example.com>
2019-11-01 11:13:01 -0500

    lowering _audio_threshold

--- thresh.py
+++ thresh.py
@@ -94,7 +94,7 @@ class bobber_bot():
 
     # [I've been keeping threshold lower on my windows box because I actually have to listen to it(?)ss]:
     if sys.platform == 'darwin':
-        _audio_threshold = 2000
+        _audio_threshold = 1600
     else:
         _audio_threshold = 200
 
